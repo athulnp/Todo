3d27ca01508c5c1ace696da288f0f4e43c62e692
athul <athulnalupurakkal@gmail.com>
2019-11-03 00:20:49 +0530

    added localstorage apis

--- src/app/service/store.service.ts
+++ src/app/service/store.service.ts
@@ -4,42 +4,43 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StoreService {
-  todoList: Array<Todo>;
-  constructor() {
-    this.todoList = [
-      {
-        id: "uuid1",
-        description: "sample description",
-        title: "Sample title"
-      },
-      {
-        id: "ffffffffff",
-        description: "sample description 4",
-        title: "sdfsd sdfsdf"
-      }
-    ]
-  }
+  private _todoList: Array<Todo>;
 
   getList() {
-    return this.todoList;
+    if (this._todoList == undefined) {
+      this._todoList = []
+      for (let i = 0; i < window.localStorage.length; i++) {
+        let id = window.localStorage.key(i)
+        let item = JSON.parse(window.localStorage.getItem(id)) as Todo
+        this._todoList.push(item)
+      }
+    }
+    return this._todoList;
   }
 
   deleteItem(id: string) {
-    // this.todoList.
+    window.localStorage.removeItem(id)
+    this._todoList = undefined
   }
 
   addItem(item: Todo) {
     console.log('saving item : ', item);
-    this.todoList.push(item)
-    console.log('list : ', this.todoList);
+    window.localStorage.setItem(item.id, JSON.stringify(item))
+    this._todoList = undefined
   }
 
   updateItem(item: Todo) {
-    for (let i of this.todoList) {
-      if (i.id === item.id) {
-        i.title = item.title,
-          i.title = item.description
-      }
-    }
+    console.log('saving item : ', item);
+    window.localStorage.setItem(item.id, JSON.stringify(item))
+    this._todoList = undefined
+    // for (let i of this.todoList) {
+    //   if (i.id === item.id) {
+    //     i.title = item.title,
+    //       i.title = item.description
+    //   }
+    // }
+  }
+  appendToEditor(item: Todo) {
+    return item;
   }
 }
