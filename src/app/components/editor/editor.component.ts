59a8691b2d25c7798f51ef28bdecf893928dc68b
athul <athulnalupurakkal@gmail.com>
2019-11-03 01:02:20 +0530

    added destroy for subscription

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, OnInit, OnDestroy, } from '@angular/core';
 import { StoreService } from 'src/app/service/store.service';
 import { TodoEditService } from 'src/app/service/todo-edit-service';
+import { Subscription } from 'rxjs';
+import { IfStmt } from '@angular/compiler';
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit {
-  todo: Todo
+export class EditorComponent implements OnInit, OnDestroy {
 
+  todo: Todo
+  editTriggerSubscription: Subscription
   constructor(private storeApi: StoreService, private todoEditSrv: TodoEditService) {
     this.reset();
   }
 
   ngOnInit() {
-    this.todoEditSrv.editTriggerEvent().subscribe(t => this.handleEdit(t))
+    this.editTriggerSubscription = this.todoEditSrv.editTriggerEvent().subscribe(t => this.handleEdit(t))
+  }
+
+  ngOnDestroy(): void {
+    if (this.editTriggerSubscription) {
+      this.editTriggerSubscription.unsubscribe();
+    }
   }
 
   canSave(): boolean {
@@ -56,4 +65,6 @@ export class EditorComponent implements OnInit {
       return v.toString(16);
     });
   }
+
+
 }
